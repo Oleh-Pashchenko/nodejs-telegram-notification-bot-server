20af50810cc64e02ec1f7bd2d6291ceeca6f178a
oleh_pashchenko <oleh.pashchenko@gmail.com>
2016-06-25 16:07:33 +0000

    update gitignore

--- modules/mongo/configurations.js
+++ modules/mongo/configurations.js
@@ -1,5 +1,16 @@
 module.exports = {
-    telegram: '',
-    database: '',
-    texts: ''
+    telegram: {
+        token: '202855970:AAGdK-DEzBKkAagF-Gl_QETcTfzB3a7tSEg',
+        options: {
+            polling: true
+        }
+    },
+    database: {
+        connectionString: 'mongodb://{0}:{1}@ds013221.mlab.com:13221/telegram-the-wall-bot',
+        user: 'the-wall-db',
+        password: 'the-wall-db2000'
+    },
+    texts: {
+        app_key_text: 'Your app key:\n'
+    }
 };
